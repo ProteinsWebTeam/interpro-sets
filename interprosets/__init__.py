d82ec04d4f14d19d0f764be8bde4016edfdab750
Matthias Blum <mat.blum@gmail.com>
2018-10-08 21:27:59 +0100

    Arguments

--- interprosets/__init__.py
+++ interprosets/__init__.py
@@ -1,50 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
-import os
-import shutil
-import tempfile
-
 
 def main():
+    import argparse
+    import utils
+
     parser = argparse.ArgumentParser(
         description="Sets/Collections in InterPro"
     )
-    parser.add_subparsers()
-    parser.add_argument("--uri",
-                        help="database connection string "
-                             "(driver:user/password@host:port/database)",
-                        required=True)
+
+    uri_arg = {
+        "help": "database connection string "
+                "(user/password@host:port/database)",
+        "required": True
+    }
+    dir_arg = {
+        "help": "temporary directory"
+    }
+    proc_arg = {
+        "help": "number of processes (default: 1)",
+        "type": int,
+        "default": 1
+    }
+
     parser.add_argument("--dir",
-                        help="working directory "
-                             "(default: temporary directory)")
+                        help="temporary directory")
     parser.add_argument("-p", dest="processes", type=int, default=1,
                         help="number of processes (default: 1)")
 
-    parsers = parser.add_subparsers()
-    _parser = parsers.add_parser("init", help="(re)create tables")
-    _parser = parsers.add_parser("cdd", help="")
-    _parser = parsers.add_parser("panther", help="")
-    _parser = parsers.add_parser("pfam", help="")
-    _parser = parsers.add_parser("pirsf", help="")
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.required = True
+    _parser = subparsers.add_parser("init", help="(re)create tables")
+    _parser.add_argument("--uri", **uri_arg)
+
+    _parser = subparsers.add_parser("cdd", help="")
+    _parser.add_argument("--uri", **uri_arg)
+    _parser.add_argument("--dir", **dir_arg)
+    _parser.add_argument("-p", **proc_arg)
+
+    _parser = subparsers.add_parser("panther", help="")
+    _parser.add_argument("--uri", **uri_arg)
+    _parser.add_argument("--dir", **dir_arg)
+    _parser.add_argument("-p", **proc_arg)
+
+    _parser = subparsers.add_parser("pfam", help="")
+    _parser.add_argument("--uri", **uri_arg)
+    _parser.add_argument("--dir", **dir_arg)
+    _parser.add_argument("-p", **proc_arg)
+
+    _parser = subparsers.add_parser("pirsf", help="")
+    _parser.add_argument("--uri", **uri_arg)
+    _parser.add_argument("--dir", **dir_arg)
+    _parser.add_argument("-p", **proc_arg)
 
     args = parser.parse_args()
 
-    if args.dir:
-        workdir = args.dir
-        delete_dir = False
-        try:
-            os.makedirs(workdir)
-        except FileExistsError:
-            pass
-    else:
-        workdir = tempfile.mkdtemp()
-        delete_dir = True
+    if args.command == "init":
+        utils.init_tables(args.uri)
+    elif args.command == "cdd":
+        pass
+    elif args.command == "panther":
+        pass
+    elif args.command == "pfam":
+        pass
+    elif args.command == "pirsf":
+        pass
 
-    processes = args.processes
-    if processes < 1:
-        processes = 1
 
-    if delete_dir:
-        shutil.rmtree(workdir)
+if __name__ == '__main__':
+    main()
+
+
