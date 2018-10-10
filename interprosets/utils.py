e5fbbfed684a1523d66c2a7d6c73b85b9e1fe59e
Matthias Blum <mat.blum@gmail.com>
2018-10-10 16:20:18 +0100

    Refactoring

--- interprosets/utils.py
+++ interprosets/utils.py
@@ -13,17 +13,100 @@ from urllib.request import urlopen
 import cx_Oracle
 
 
-def logger(msg):
-    sys.stderr.write(
-        "{:%Y-%m-%d %H:%M:%S}: {}\n".format(datetime.now(), msg)
-    )
-    sys.stderr.flush()
+def batch_compass(jobs, processes=1):
+    return _batch(_compass, jobs, processes)
+
+
+def batch_hmmconvert(jobs, processes=1):
+    return _batch(_hmmconvert, jobs, processes)
+
+
+def batch_hmmscan(jobs, processes=1):
+    return _batch(_hmmscan, jobs, processes)
+
+
+def compass(fasta_file, profile_db):
+    out_file = fasta_file[:-2] + "out"
+    cmd = [
+        "compass_vs_db",
+        "-i", fasta_file,
+        "-d", profile_db,
+        "-o", out_file
+    ]
+    Popen(cmd, stdout=DEVNULL, stderr=DEVNULL).wait()
+
+    return fasta_file, out_file
 
 
 def connect(uri):
     # Format: user/password@host[:port/schema]
     con = cx_Oracle.connect(uri)
     return con, con.cursor()
+
+
+def download(url, dst):
+    with urlopen(url) as res, open(dst, "wb") as fh:
+        while True:
+            data = res.read(1024)
+            if data:
+                fh.write(data)
+            else:
+                break
+
+
+def extract(src, dst):
+    with open(dst, "wt") as fh:
+        for line in iterlines(src):
+            fh.write(line)
+
+
+def hmmconvert(hmm_file):
+    hmm = ""
+    pop = Popen(
+        ["hmmconvert", hmm_file], stdout=PIPE, stderr=DEVNULL
+    )
+    for line in pop.stdout:
+        hmm += line.decode("utf-8")
+
+    out, err = pop.communicate()
+    return hmm
+
+
+def hmmemit(hmm_file, fasta_file):
+    return Popen(
+        ["hmmemit", "-c", "-o", fasta_file, hmm_file],
+        stdout=DEVNULL, stderr=DEVNULL
+    ).wait() == 0
+
+
+def hmmpress(hmm_db):
+    files = [hmm_db + ext for ext in (".h3m", ".h3i", ".h3f", ".h3p")]
+
+    for f in files:
+        try:
+            os.remove(f)
+        except FileNotFoundError:
+            pass
+
+    p = Popen(["hmmpress", hmm_db], stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+
+    if p.returncode != 0:
+        raise RuntimeError("{}\n------\n{}".format(out, err))
+
+
+def hmmscan(fasta_file, hmm_db):
+    tab_file = fasta_file[:-2] + 'tab'
+    out_file = fasta_file[:-2] + 'out'
+
+    with open(out_file, 'wt') as fh:
+        # (?) option for --cut_ga and -E
+        Popen(
+            ["hmmscan", "--domtblout", tab_file, hmm_db, fasta_file],
+            stdout=fh, stderr=DEVNULL
+        ).wait()
+
+    return fasta_file, out_file, tab_file
 
 
 def init_tables(uri):
@@ -99,14 +182,131 @@ def init_tables(uri):
     )
 
 
-def download(url, dst):
-    with urlopen(url) as res, open(dst, "wb") as fh:
-        while True:
-            data = res.read(1024)
-            if data:
-                fh.write(data)
+def iterlines(filepath):
+    if filepath.lower().endswith(".gz"):
+        fn = gzip.open
+    else:
+        fn = open
+
+    with fn(filepath, "rt") as fh:
+        for line in fh:
+            yield line
+
+
+def logger(msg):
+    sys.stderr.write(
+        "{:%Y-%m-%d %H:%M:%S}: {}\n".format(datetime.now(), msg)
+    )
+    sys.stderr.flush()
+
+
+def mk_compass_db(files_list, profile_db):
+    return Popen(
+        ["mk_compass_db", "-i", files_list, "-o", profile_db],
+        stdout=DEVNULL,
+        stderr=DEVNULL
+    ).wait() == 0
+
+
+def parse_compass_results(out_file):
+    p1 = re.compile(r"length\s*=\s*(\d+)")
+    p2 = re.compile(r"Evalue\s*=\s*([\d.e\-]+)")
+
+    targets = {}
+    block = 0
+    query_id = None
+    query_seq = ""
+    target_id = None
+    target_seq = ""
+    length = None
+    evalue = None
+    pos_start = None
+
+    it = iterlines(out_file)
+    for line in it:
+        line = line.rstrip()
+        if line.startswith("Subject="):
+            """
+            Format:
+            Subject= cd154/cd15468.fa
+            length=413	filtered_length=413	Neff=1.000
+            Smith-Waterman score = 254	Evalue = 3.36e-16
+            
+            (the path after "Subject=" might be truncated)
+            """
+            if target_id:
+                targets[target_id] = {
+                    "id": target_id,
+                    "evalue": evalue,
+                    "length": length,
+                    "start": pos_start,
+                    "end": pos_start + length - 1,
+                    "sequences": {
+                        "query": query_seq,
+                        "target": target_seq
+                    }
+                }
+
+            query_id = None
+            query_seq = None
+            target_id = None
+            target_seq = None
+
+            line = next(it)
+            length = int(p1.match(line).group(1))
+
+            line = next(it)
+            try:
+                evalue = float(p2.search(line).group(1))
+            except ValueError:
+                evalue = 0
+
+            block = 1
+        elif line.startswith("Parameters:"):
+            # Footer: end of results
+            break
+        elif not block:
+            continue
+        elif line:
+            """
+            First block:           
+            gnl|CDD|271233   1      PSFIPGPT==TPKGCTRIPSFSLSDTHWCYTHNVILSGCQDHSKSNQYLSLGVIKTNSDG
+            CONSENSUS_1      1      PSFIPGPT==TPKGCTRIPSFSLSDTHWCYTHNVILSGCQDHSKSNQYLSLGVIKTNSDG
+                                    P++IP+ T      C+R PSF++S+  + YT+ V  ++CQDH +  +Y+++GVI+ ++ G
+            CONSENSUS_2      1      PNLIPADTGLLSGECVRQPSFAISSGIYAYTYLVRKGSCQDHRSLYRYFEVGVIRDDGLG
+            gnl|CDD|271230   1      PNLIPADTGLLSGECVRQPSFAISSGIYAYTYLVRKGSCQDHRSLYRYFEVGVIRDDGLG
+            
+            (following blocks do not have the start position between the ID and the sequence)
+            """
+            query = line.split()
+            next(it)
+            next(it)
+            next(it)
+            target = line.split()
+
+            if block == 1:
+                query_id = query[0]
+                pos_start = int(query[1])
+                query_seq = query[2]
+                target_id = target[0]
+                target_seq = target[2]
             else:
-                break
+                query_seq += query[1]
+                target_seq += target[1]
+
+    targets[target_id] = {
+        "id": target_id,
+        "evalue": evalue,
+        "length": length,
+        "start": pos_start,
+        "end": pos_start + length - 1,
+        "sequences": {
+            "query": query_seq,
+            "target": target_seq
+        }
+    }
+
+    return list(targets.values())
 
 
 def parse_hmm(filepath, keep_hmm=True):
@@ -143,181 +343,6 @@ def parse_hmm(filepath, keep_hmm=True):
         logger("WARNING: {} duplicated entries".format(len(duplicates)))
 
     return entries
-
-
-def extract(src, dst):
-    with open(dst, "wt") as fh:
-        for line in iterlines(src):
-            fh.write(line)
-
-
-def iterlines(filepath):
-    if filepath.lower().endswith(".gz"):
-        fn = gzip.open
-    else:
-        fn = open
-
-    with fn(filepath, "rt") as fh:
-        for line in fh:
-            yield line
-
-
-def read_fasta(filepath):
-    seq = ""
-    with open(filepath, "rt") as fh:
-        next(fh)
-
-        for line in fh:
-            seq += line.rstrip()
-
-    return seq
-
-
-def hmmemit(hmm_file, fasta_file):
-    try:
-        os.mkdir(os.path.dirname(fasta_file))
-    except FileExistsError:
-        pass
-    except FileNotFoundError as e:
-        # Parent directory does not exist
-        raise e
-
-    return Popen(
-        ["hmmemit", "-c", "-o", fasta_file, hmm_file],
-        stdout=DEVNULL, stderr=DEVNULL
-    ).wait() == 0
-
-
-def hmmpress(hmm_db):
-    files = [hmm_db + ext for ext in (".h3m", ".h3i", ".h3f", ".h3p")]
-
-    for f in files:
-        try:
-            os.remove(f)
-        except FileNotFoundError:
-            pass
-
-    p = Popen(["hmmpress", hmm_db], stdout=PIPE, stderr=PIPE)
-    out, err = p.communicate()
-
-    if p.returncode != 0:
-        raise RuntimeError("{}\n------\n{}".format(out, err))
-
-
-def hmmconvert(hmm_file):
-    hmm = ""
-    pop = Popen(
-        ["hmmconvert", hmm_file], stdout=PIPE, stderr=DEVNULL
-    )
-    for line in pop.stdout:
-        hmm += line.decode("utf-8")
-
-    out, err = pop.communicate()
-    return hmm
-
-
-def _hmmconvert(args):
-    acc, hmm_file = args
-    return acc, hmmconvert(hmm_file)
-
-
-def _batch(func, jobs, processes):
-    if processes > 1:
-        with Pool(processes) as pool:
-            for res in pool.imap_unordered(func, jobs):
-                yield res
-    else:
-        for res in map(func, jobs):
-            yield res
-
-
-def hmmscan(acc, fasta_file, hmm_db):
-    tab_file = fasta_file[:-2] + 'tab'
-    out_file = fasta_file[:-2] + 'out'
-
-    with open(out_file, 'wt') as fh:
-        # (?) option for --cut_ga and -E
-        Popen(
-            ["hmmscan", "--domtblout", tab_file, hmm_db, fasta_file],
-            stdout=fh, stderr=DEVNULL
-        ).wait()
-
-    return acc, fasta_file, out_file, tab_file
-
-
-def _hmmscan(args):
-    return hmmscan(*args)
-
-
-def batch_hmmconvert(jobs, processes=1):
-    return _batch(_hmmconvert, jobs, processes)
-
-
-def batch_hmmscan(jobs, processes=1):
-    return _batch(_hmmscan, jobs, processes)
-
-
-def parse_block(fh, line):
-    block = []
-    while line or len(block) < 4:
-        block.append(line)
-        line = next(fh).strip()
-
-    i = 0
-    for i, line in enumerate(block):
-        if len(line.split()) == 4:
-            break
-
-    target = block[i].split()[2]
-    query = block[i+2].split()[2]
-
-    return target, query
-
-
-def _parse_hmmscan_alignments(filepath):
-    domains = []
-    target = ""
-    query = ""
-    # p_dom = re.compile(
-    #     r"== domain (\d+)\s+score: ([\d.]+) bits;\s*conditional E-value: ([\d.\-e]+)"
-    # )
-    n_blank = 0
-    with open(filepath, "rt") as fh:
-        for line in fh:
-            line = line.strip()
-            if line:
-                n_blank = 0
-            else:
-                n_blank += 1
-
-            if line.startswith("== domain"):
-                # new domain: flush previous one
-                if target:
-                    domains.append({"target": target, "query": query})
-                    target = ""
-                    query = ""
-
-                t, q = parse_block(fh, next(fh).strip())
-                target += t
-                query += q
-
-            elif line.startswith(">>"):
-                # new complete sequence: flush previous domain
-                if target:
-                    domains.append({"target": target, "query": query})
-                    target = ""
-                    query = ""
-            elif target:
-                if line:
-                    t, q = parse_block(fh, line)
-                    target += t
-                    query += q
-                elif n_blank == 2:
-                    domains.append({"target": target, "query": query})
-                    target = ""
-                    query = ""
-
-    return domains
 
 
 def parse_hmmscan_results(out_file, tab_file):
@@ -386,3 +411,106 @@ def parse_hmmscan_results(out_file, tab_file):
         i += 1
 
     return list(targets.values())
+
+
+def read_fasta(filepath, reo=None):
+    it = iterlines(filepath)
+    line = next(it)
+    if reo:
+        m = reo.match(line)
+    else:
+        m = None
+
+    seq = ""
+    for line in it:
+        seq += line.rstrip()
+
+    return seq, m
+
+
+def _batch(func, jobs, processes):
+    if processes > 1:
+        with Pool(processes) as pool:
+            for res in pool.imap_unordered(func, jobs):
+                yield res
+    else:
+        for res in map(func, jobs):
+            yield res
+
+
+def _compass(args):
+    acc, fasta_file, profile_db = args
+    return acc, *compass(fasta_file, profile_db)
+
+
+def _hmmconvert(args):
+    acc, hmm_file = args
+    return acc, hmmconvert(hmm_file)
+
+
+def _hmmscan(args):
+    acc, fasta_file, hmm_db = args
+    return acc, *hmmscan(fasta_file, hmm_db)
+
+
+def _parse_block(fh, line):
+    block = []
+    while line or len(block) < 4:
+        block.append(line)
+        line = next(fh).strip()
+
+    i = 0
+    for i, line in enumerate(block):
+        if len(line.split()) == 4:
+            break
+
+    target = block[i].split()[2]
+    query = block[i+2].split()[2]
+
+    return target, query
+
+
+def _parse_hmmscan_alignments(filepath):
+    domains = []
+    target = ""
+    query = ""
+    # p_dom = re.compile(
+    #     r"== domain (\d+)\s+score: ([\d.]+) bits;\s*conditional E-value: ([\d.\-e]+)"
+    # )
+    n_blank = 0
+    with open(filepath, "rt") as fh:
+        for line in fh:
+            line = line.strip()
+            if line:
+                n_blank = 0
+            else:
+                n_blank += 1
+
+            if line.startswith("== domain"):
+                # new domain: flush previous one
+                if target:
+                    domains.append({"target": target, "query": query})
+                    target = ""
+                    query = ""
+
+                t, q = _parse_block(fh, next(fh).strip())
+                target += t
+                query += q
+
+            elif line.startswith(">>"):
+                # new complete sequence: flush previous domain
+                if target:
+                    domains.append({"target": target, "query": query})
+                    target = ""
+                    query = ""
+            elif target:
+                if line:
+                    t, q = _parse_block(fh, line)
+                    target += t
+                    query += q
+                elif n_blank == 2:
+                    domains.append({"target": target, "query": query})
+                    target = ""
+                    query = ""
+
+    return domains
