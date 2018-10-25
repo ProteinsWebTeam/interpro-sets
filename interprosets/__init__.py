b7a0ef15f9ec6377d99c3e98d05507cb9e3feea9
Matthias Blum <mat.blum@gmail.com>
2018-10-25 12:17:28 +0100

    Wrap all script in a temporary directory context

--- interprosets/__init__.py
+++ interprosets/__init__.py
@@ -5,7 +5,7 @@
 def cli():
     import argparse
     import os
-    from tempfile import gettempdir
+    import tempfile
 
     from . import cdd, panther, pfam, pirsf, utils
 
@@ -20,7 +20,7 @@ def cli():
 
     dir_arg = {
         "help": "temporary directory",
-        "default": gettempdir()
+        "default": tempfile.gettempdir()
     }
     proc_arg = {
         "help": "number of processes (default: 1)",
@@ -36,26 +36,38 @@ def cli():
 
     subparsers = parser.add_subparsers(dest="command")
     subparsers.required = True
-    _parser = subparsers.add_parser("init", help="(re)create tables")
+    subparsers.add_parser("init", help="(re)create tables")
 
-    _parser = subparsers.add_parser("cdd", help="CDD profile-profile alignments with COMPASS")
+    _parser = subparsers.add_parser(
+        "cdd", help="CDD profile-profile alignments with COMPASS"
+    )
     _parser.add_argument("--dir", **dir_arg)
     _parser.add_argument("-p", **proc_arg)
-    _parser.add_argument("--sequences", help="FASTA file of representative sequences")
-    _parser.add_argument("--links", help="list of superfamilies and their domain models")
+    _parser.add_argument("--sequences",
+                         help="FASTA file of representative sequences")
+    _parser.add_argument("--links",
+                         help="list of superfamilies and their domain models")
 
-    _parser = subparsers.add_parser("panther", help="")
+    _parser = subparsers.add_parser(
+        "panther", help="PANTHER profile-profile alignments with HMMSCAN"
+    )
     _parser.add_argument("--dir", **dir_arg)
     _parser.add_argument("-p", **proc_arg)
-    _parser.add_argument("--books", help="directory of 'books' (protein families)", required=True)
+    _parser.add_argument("--books",
+                         help="directory of 'books' (protein families)",
+                         required=True)
 
-    _parser = subparsers.add_parser("pfam", help="Pfam profile-profile alignments with HMMSCAN")
+    _parser = subparsers.add_parser(
+        "pfam", help="Pfam profile-profile alignments with HMMSCAN"
+    )
     _parser.add_argument("--dir", **dir_arg)
     _parser.add_argument("-p", **proc_arg)
     _parser.add_argument("--hmm", help="Pfam-A HMM file")
     _parser.add_argument("--clans", help="Pfam clans TSV file")
 
-    _parser = subparsers.add_parser("pirsf", help="")
+    _parser = subparsers.add_parser(
+        "pirsf", help="PIRFS profile-profile alignments with HMMSCAN"
+    )
     _parser.add_argument("--dir", **dir_arg)
     _parser.add_argument("-p", **proc_arg)
     _parser.add_argument("--hmm", help="PIRSF HMM file", required=True)
@@ -65,16 +77,38 @@ def cli():
 
     if args.command == "init":
         utils.init_tables(uri)
-    elif args.command == "cdd":
-        cdd.run(uri, cdd_masters=args.sequences,
-                links=args.links, processes=args.processes,
-                tmpdir=args.dir)
-    elif args.command == "panther":
-        panther.run(uri, args.books,
-                    tmpdir=args.dir, processes=args.processes)
-    elif args.command == "pfam":
-        pfam.run(uri, hmm_db=args.hmm, clans_tsv=args.clans,
-                 processes=args.processes, tmpdir=args.dir)
-    elif args.command == "pirsf":
-        pirsf.run(uri, args.hmm, pirsfinfo=args.info,
-                  tmpdir=args.dir, processes=args.processes)
+    else:
+        os.makedirs(args.dir, exist_ok=True)
+
+        with tempfile.TemporaryDirectory(dir=args.dir) as tmpdir:
+            if args.command == "cdd":
+                cdd.run(uri,
+                        cdd_masters=args.sequences,
+                        links=args.links,
+                        processes=args.processes,
+                        tmpdir=tmpdir)
+
+            elif args.command == "panther":
+                panther.run(uri, args.books,
+                            tmpdir=tmpdir,
+                            processes=args.processes)
+
+            elif args.command == "pfam":
+                pfam.run(uri,
+                         hmm_db=args.hmm,
+                         clans_tsv=args.clans,
+                         processes=args.processes,
+                         tmpdir=tmpdir)
+
+            elif args.command == "pirsf":
+                pirsf.run(uri, args.hmm,
+                          pirsfinfo=args.info,
+                          tmpdir=tmpdir,
+                          processes=args.processes)
+
+            size = 0
+            for root, dirs, files in os.walk(tmpdir):
+                for f in files:
+                    size += os.path.getsize(os.path.join(root, f))
+
+            utils.logger("temporary files: {} bytes".format(size))
